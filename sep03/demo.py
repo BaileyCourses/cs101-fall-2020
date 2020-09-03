0fd473487154d81e71e10812f1db7dd6b60c9a69
Mark Bailey <mbailey@hamilton.edu>
2020-09-03 12:47:27 -0400

    Code from Thursday, September 3

---
+++ sep03/demo.py
@@ -0,0 +1,26 @@
+#
+# Long live [Lists!]
+#
+# September 3, 2020
+#
+
+from turtle import *
+from random import randrange
+
+def main():
+    fruits = ["apple", "pear", "banana", "peach"]
+    
+    print(fruits[-2:])
+
+#    college ="Hamilton College"
+#    
+#    print(college.find("l", -7, -1))
+    
+#    for i in range(len(college)):
+#        print(i, len(college[i]))
+    
+    
+    
+
+if __name__ == "__main__":
+    main()
