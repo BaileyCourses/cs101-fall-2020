c46de2a6dbda5033f980fc610a6d843d685d7083
Mark Bailey <mbailey@hamilton.edu>
2020-09-10 12:46:55 -0400

    Sep. 8

---
+++ sep08/demo.py
@@ -0,0 +1,58 @@
+#
+# Lists and such
+#
+# September 8, 2020
+#
+from random import *
+from cs101audio *
+
+
+def main():
+    song = Audio()
+    song.open_audio_file("america_the_beautiful.wav")
+    song.play()
+#     numNames = int(input("How many names do you have? "))
+#     
+#     names = []
+#     for _ in range(numNames):
+#         name = input("Enter a name: ")
+#         names.append(name)
+#         
+#     print(names)
+        
+#     # age = "8h10 13]"
+#     ages = "8 10 13" #[8, 10, 13]
+#     keep = ages
+# 
+#     print("Ages is now:", ages)
+#     print("Keep is now:", keep)
+# 
+# #    ages = [9, 11, 14]
+# #    ages[1] = 11
+# #    ages[1] = "h"
+#     ages = ages[:1] + "h" + ages[2:]
+#     
+#     print("Ages is now:", ages)
+#    print("Keep is now:", keep)
+    
+
+
+#    print("The number I'm thinking of is:", randrange(10))
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+#other stuff:
+#    song = Audio()
+#    song.open_audio_file("america_the_beautiful.wav")
+#    song.play()
